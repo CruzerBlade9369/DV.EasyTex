c27ee893a3ecb444a61ca3e55b1133357cc870cf
CruzerBlade9369 <pradiparasendrya@gmail.com>
2026-01-17 13:38:00 +0800

    passenger car support

--- Source/EasyTex/Main.cs
+++ Source/EasyTex/Main.cs
@@ -1,6 +1,5 @@
 ﻿using HarmonyLib;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityModManagerNet;
@@ -13,24 +12,31 @@ namespace EasyTex
 		static UnityModManager.ModEntry _mod;
 		static AssetBundle bundle;
 		static GameObject[] patchedPrefabs = { null };
-		static bool slicedCarsInstalled = false;
+		//static bool slicedCarsInstalled = false;
 
 		static void Load(UnityModManager.ModEntry mod)
 		{
 			// Setup UMM
 			_mod = mod;
 
-			slicedCarsInstalled = Directory.Exists(Path.Combine(mod.Path, "../SlicedPassengerCars"));
+			// Sliced passenger cars is obsolete
+			/*slicedCarsInstalled = Directory.Exists(Path.Combine(mod.Path, "../SlicedPassengerCars"));
 			if (slicedCarsInstalled)
-				mod.Logger.Warning("Sliced Passenger Cars mod detected! Patched passenger cars have been disabled.");
+				mod.Logger.Warning("Sliced Passenger Cars mod detected! Patched passenger cars have been disabled.");*/
+			
 			// Setup the harmony patches
 			Harmony harmony = new Harmony(_mod.Info.Id);
 			harmony.Patch(
 				original: AccessTools.Method(typeof(TrainCar), nameof(TrainCar.GetCarPrefab)),
 				postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.GetCarPrefab_Patch)));
 
+			DebugLog("Started");
+        }
+
+		static void DebugLog(string txt)
+		{
 #if DEBUG
-			_mod.Logger.Log("[EasyTex] Started");
+			_mod.Logger.Log("[EasyTex] " + txt);
 #endif
 		}
 
@@ -40,7 +46,9 @@ namespace EasyTex
 				"car_tanker_lod/car_tanker_LOD0",
 				"car_tanker_lod/car_tanker_LOD1",
 				"car_tanker_lod/car_tanker_LOD2",
-				"car_tanker_lod/car_tanker_LOD3"};
+				"car_tanker_lod/car_tanker_LOD3"
+			};
+
 			private static string[] carTankLODMap_Sim = new string[]
 			{
 				"CarTank/CarTank_LOD0",
@@ -48,40 +56,52 @@ namespace EasyTex
 				"CarTank/CarTank_LOD2",
 				"CarTank/CarTank_LOD3"
 			};
+
 			private static string[] cabooseLODMap = new string[] {
 				"CarCaboose_exterior/CabooseExterior",
 				"CarCaboose_exterior/CabooseExterior_LOD1",
 				"CarCaboose_exterior/CabooseExterior_LOD2",
 				"CarCaboose_exterior/Caboose_LOD3"
 			};
+
 			private static string[] flatCarLODMap = new string[] {
 				"car_flatcar_lod/flatcar",
 				"car_flatcar_lod/car_flatcar_LOD1",
 				"car_flatcar_lod/car_flatcar_LOD2",
-				"car_flatcar_lod/car_flatcar_LOD3"};
+				"car_flatcar_lod/car_flatcar_LOD3"
+			};
+
 			private static string[] boxCarLODMap = new string[] {
 				"car_boxcar_lod/car_boxcar",
 				"car_boxcar_lod/car_boxcar_LOD1",
 				"car_boxcar_lod/car_boxcar_LOD2",
 				"car_boxcar_lod/car_boxcar_LOD3"
 			};
+
 			private static string[] boxCarLODMap_Sim = new string[] {
 				"CarBoxcar/CarBoxcar_LOD0",
 				"CarBoxcar/CarBoxcar_LOD1",
 				"CarBoxcar/CarBoxcar_LOD2",
 				"CarBoxcar/CarBoxcar_LOD3"
 			};
+
 			private static string[] passengerLODMap = new string[]
 			{
-				"car_passenger_lod/passenger_car/exterior",
-				"car_passenger_lod/passenger_car/front doors",
-				"car_passenger_lod/passenger_car/side doors",
-				"car_passenger_lod/car_passenger_LOD1",
+                "car_passenger_lod/car_passenger_LOD0",
+                "car_passenger_lod/car_passenger_LOD1",
 				"car_passenger_lod/car_passenger_LOD2",
 				"car_passenger_lod/car_passenger_LOD3"
 			};
 
-			private static string[] patchedPrefabList = new string[]
+            private static string[] passengerLODMap_Sim = new string[]
+            {
+                "CarPassenger/CarPassenger_LOD0",
+                "CarPassenger/CarPassenger_LOD1",
+                "CarPassenger/CarPassenger_LOD2",
+                "CarPassenger/CarPassenger_LOD3"
+            };
+
+            private static string[] patchedPrefabList = new string[]
 			{
 				"Patched_CarTanker",
 				"Patched_CarCaboose",
@@ -101,31 +121,31 @@ namespace EasyTex
 					case TrainCarType.TankOrange:
 					case TrainCarType.TankWhite:
 					case TrainCarType.TankYellow:
-#if DEBUG
-						_mod.Logger.Log("[EasyTex] Getting Tank Car LODs");
-#endif
+						DebugLog("Getting Tank Car LODs");
 						return carTankLODMap;
+
 					case TrainCarType.CabooseRed:
-#if DEBUG
-						_mod.Logger.Log("[EasyTex] Getting Caboose LODs");
-#endif
+						DebugLog("Getting Caboose LODs");
 						return cabooseLODMap;
+
 					// Disabled, but I'm not willing to remove it completely
 					//case TrainCarType.FlatbedEmpty:
 					//	return flatCarLODMap;
+
 					case TrainCarType.BoxcarBrown:
 					case TrainCarType.BoxcarGreen:
 					case TrainCarType.BoxcarPink:
 					case TrainCarType.BoxcarRed:
-#if DEBUG
-						_mod.Logger.Log("[EasyTex] Getting Box Car LODs");
-#endif
+						DebugLog("Getting Box Car LODs");
 						return boxCarLODMap;
-					// Needs reworked
-					//case TrainCarType.PassengerBlue:
-					//case TrainCarType.PassengerGreen:
-					//case TrainCarType.PassengerRed:
-					//	return passengerLODMap;
+
+					// Reworked, maybe works now?
+					case TrainCarType.PassengerBlue:
+					case TrainCarType.PassengerGreen:
+					case TrainCarType.PassengerRed:
+						DebugLog("Getting Passenger Car LODs");
+                        return passengerLODMap;
+
 					default:
 						return null;
 				}
@@ -142,30 +162,30 @@ namespace EasyTex
 					case TrainCarType.TankOrange:
 					case TrainCarType.TankWhite:
 					case TrainCarType.TankYellow:
-#if DEBUG
-						_mod.Logger.Log("[EasyTex] Getting Tank Car LODs");
-#endif
-						return carTankLODMap_Sim;
+                        DebugLog("Sim - Getting Tank Car LODs");
+                        return carTankLODMap_Sim;
+
 					case TrainCarType.CabooseRed:
-#if DEBUG
-						_mod.Logger.Log("[EasyTex] Getting Caboose LODs");
-#endif
-						return cabooseLODMap;
+                        DebugLog("Sim - Getting Caboose LODs");
+                        return cabooseLODMap;
+
 					// Disabled, but I'm not willing to remove it completely
 					//case TrainCarType.FlatbedEmpty:
 					//	return flatCarLODMap;
+
 					case TrainCarType.BoxcarBrown:
 					case TrainCarType.BoxcarGreen:
 					case TrainCarType.BoxcarPink:
 					case TrainCarType.BoxcarRed:
-#if DEBUG
-						_mod.Logger.Log("[EasyTex] Getting Box Car LODs");
-#endif
-						return boxCarLODMap_Sim;
+                        DebugLog("Sim - Getting Box Car LODs");
+                        return boxCarLODMap_Sim;
+
 					case TrainCarType.PassengerBlue:
 					case TrainCarType.PassengerGreen:
 					case TrainCarType.PassengerRed:
-						//return passengerLODMap;
+                        DebugLog("Sim - Getting Passenger Car LODs");
+                        return passengerLODMap_Sim;
+
 					default:
 						return null;
 				}
@@ -187,21 +207,23 @@ namespace EasyTex
 						patchedPrefabs[i] = (GameObject)bundle.LoadAsset(patchedPrefabList[i]);
 					}
 				}
-#if DEBUG
-				_mod.Logger.Log("[EasyTex] Loaded " + patchedPrefabs.Length + "Patches");
+
+				DebugLog("Loaded " + patchedPrefabList.Length + "Patches");
+
 				foreach(GameObject go in patchedPrefabs)
-					_mod.Logger.Log("[EasyTex] " + go.name);
-#endif
+				{
+					DebugLog(go.name);
+				}
 			}
 
 			// Return a patchec prefab based on the specified carType
 			static GameObject GetPatchedPrefab(TrainCarType carType)
 			{
 				if (patchedPrefabs[0] == null) LoadPrefabs();
-#if DEBUG
-				_mod.Logger.Log("[EasyTex] Patching a " + carType.ToString());
-#endif
-				switch (carType)
+
+				DebugLog("Patching a " + carType.ToString());
+
+                switch (carType)
 				{
 					case TrainCarType.TankBlack:
 					case TrainCarType.TankBlue:
@@ -210,20 +232,25 @@ namespace EasyTex
 					case TrainCarType.TankWhite:
 					case TrainCarType.TankYellow:
 						return patchedPrefabs[0];
+
 					case TrainCarType.CabooseRed:
 						return patchedPrefabs[1];
+
 					// Disabled, but I'm not willing to remove it completely
 					//case TrainCarType.FlatbedEmpty:
 					//	return patchedPrefabs[2];
+
 					case TrainCarType.BoxcarBrown:
 					case TrainCarType.BoxcarGreen:
 					case TrainCarType.BoxcarPink:
 					case TrainCarType.BoxcarRed:
 						return patchedPrefabs[3];
+
 					case TrainCarType.PassengerBlue:
 					case TrainCarType.PassengerGreen:
 					case TrainCarType.PassengerRed:
 						return patchedPrefabs[4];
+
 					default:
 						return null;
 				}
@@ -240,22 +267,20 @@ namespace EasyTex
 					// Get the traincar component
 					TrainCar tc = __result.GetComponent<TrainCar>();
 
-					if ((
+					/*if ((
 							tc.carType == TrainCarType.PassengerBlue ||
 							tc.carType == TrainCarType.PassengerGreen ||
 							tc.carType == TrainCarType.PassengerRed
 						) &&
 						slicedCarsInstalled)
-						return;
+						return;*/
 
 					// Get the LOD tree for the carType, return on invalid carType
 					string[] lods = GetLODs(tc.carType);
 					string[] lods_sim = GetLODs_Sim(tc.carType);
 					if (lods == null || lods_sim == null)
-					{
-#if DEBUG
-						_mod.Logger.Log("[EasyTex] LODs null return");
-#endif
+					{ 
+						DebugLog("LODs null return");
 						return;
 					}
 
